6e4d2a8b8806456e6e353e8e266c25b3c3764bf9
Juho Härme <juho.harme@uta.fi>
2016-05-21 16:38:19 +0300

    abstracting db operations

--- auth/login_submit.php
+++ auth/login_submit.php
@@ -2,51 +2,59 @@
 
 session_start();
 
-function validate_length($inputs){
-    //check that pw and usrname are of valid lengths
-    //inputs = array
-    foreach($inputs as $input){
-         $valid = (strlen($input)>20 || strlen($input)<4 || !ctype_alnum($input) ? false : true);
-         if (!$valid)
-             return false;
+function validate_login(){
+    //check that pw and usrname are set, are of valid lengths, contain only alphanumeric chars
+    $valid = true;
+    foreach(func_get_args() as $input){
+        if (!isset($input) || strlen($input)>20 || strlen($input)<4 || !ctype_alnum($input) )
+            $valid = false;
     }
-    return true;
+    return $valid;
 }
 
-function validate_login($uname,$pw){
 
-    echo $_POST["username"];
-    if (!isset($uname,$pw))
-        return "Et antanut käyttäjätunnusta tai salasanaa!";
-    if (!validate_length(func_get_args()))
-        return "Käyttäjätunnus tai salasana eivät kelpaa";
-    elseif (ctype_alnum($_POST['username']) != true)
+$msg = 'Kirjautuminen epäonnistui';
+$valid = validate_login($_POST["username"],$_POST["password"]);
 
-    return "OK";
-
-}
-
-$msg = validate_login($_POST["username"],$_POST["password"]);
-
-if(isset( $_SESSION['user_id'] ))
+if (isset( $_SESSION['user_id'] )){
     $msg = 'Kirjautuminen voimassa.';
+}
+elseif ($valid){
+
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+    $password = sha1( $password ); //encrypt
+    $mysql_hostname = 'localhost';
+    $mysql_username = 'testuser';
+    $mysql_password = 'testpw';
+    $mysql_dbname = 'majakka_auth';
+
+    try {
+        $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+        // set the error mode to exceptions
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // prepare and bind
+        $stmt = $dbh->prepare("SELECT user_id, username, password FROM majakka_users 
+                    WHERE username = :username AND password = :password");
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
+
+        $stmt->execute();
+        $user_id = $stmt->fetchColumn();
+
+        if($user_id){
+            $_SESSION['user_id'] = $user_id;
+            $msg = 'Kirjautuminen onnistui';
+        }
+    }
+    catch(Exception $e) {
+        $msg = 'Virhe kirjautumisessa:' . $e;
+    }
 
 }
 
-/*** check the username has only alpha numeric characters ***/
-elseif (ctype_alnum($_POST['username']) != true)
-{
-    /*** if there is no match ***/
-    $message = "Username must be alpha numeric";
-}
-/*** check the password has only alpha numeric characters ***/
-elseif (ctype_alnum($_POST['password']) != true)
-{
-        /*** if there is no match ***/
-        $message = "Password must be alpha numeric";
-}
 
-$message = "moro " . $_POST["username"] . "!";
 ?>
 
 <html lang="fi">
