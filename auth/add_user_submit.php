8643e268b129b63dba3b03660768c7a5119c0831
Juho Härme <juho.harme@uta.fi>
2016-11-07 22:47:02 +0200

    korjaa käyttäjälisäys käyttämään omia olioita

--- auth/add_user_submit.php
+++ auth/add_user_submit.php
@@ -4,84 +4,53 @@ require('../phputils/dbutils.php');
 
 if(!isset($_POST['username'], $_POST['password'], $_POST['form_token']))
 {
-    var_dump($_POST);
     $message = 'Please enter a valid username and password';
 }
-/*** check the form token is valid ***/
 elseif( $_POST['form_token'] != $_SESSION['form_token'])
 {
     $message = 'Invalid form submission';
 }
-/*** check the username is the correct length ***/
 elseif (strlen( $_POST['username']) > 20 || strlen($_POST['username']) < 4)
 {
     $message = 'Incorrect Length for Username';
 }
-/*** check the password is the correct length ***/
 elseif (strlen( $_POST['password']) > 20 || strlen($_POST['password']) < 4)
 {
     $message = 'Incorrect Length for Password';
 }
-/*** check the username has only alpha numeric characters ***/
 elseif (ctype_alnum($_POST['username']) != true)
 {
-    /*** if there is no match ***/
     $message = "Username must be alpha numeric";
 }
 
-/*** check the password has only alpha numeric characters ***/
 elseif (ctype_alnum($_POST['password']) != true)
 {
-        /*** if there is no match ***/
         $message = "Password must be alpha numeric";
 }
 else
 {
-    /*** if we are here the data is valid and we can insert it into database ***/
     $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
-    /*** now we can encrypt the password ***/
+    #encrypt the password
     $password = sha1( $password );
     
-    /*** connect to database ***/
-    /*** mysql hostname ***/
-    $mysql_hostname = 'localhost';
-
-    /*** mysql username ***/
-    $mysql_username = '786688';
-
-    /*** mysql password ***/
-    $mysql_password = 'Qlju16su';
-
-    /*** database name ***/
-    $mysql_dbname = '786688';
-
+    $con = new DbCon();
 
     try
     {
-        $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
-
-        /*** set the error mode to excptions ***/
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $stmt = $dbh->prepare("INSERT INTO majakka_users (username, password ) VALUES (:username, :password )");
-        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-        $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
-        $stmt->execute();
-        unset( $_SESSION['form_token'] );
+        $con->insert("majakka_users", Array("username"=>$username,"password"=>$password));
+        unset($_SESSION['form_token']);
         $message = 'New user added';
     }
     catch(Exception $e)
     {
-        /*** check if the username already exists ***/
         if( $e->getCode() == 23000)
         {
             $message = 'Username already exists';
         }
         else
         {
-            /*** if we are here, something has gone wrong with the database ***/
             $message = "$e";
         }
     }
