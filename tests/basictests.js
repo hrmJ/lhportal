93fd26d45e583a2dcc87c3a3ea5789785129eae2
Juho Härme <juho.harme@uta.fi>
2017-05-15 19:35:17 +0300

    put browser opener to before

--- tests/basictests.js
+++ tests/basictests.js
@@ -2,21 +2,33 @@ var Nightmare = require('nightmare');
 var expect = require('chai').expect; // jshint ignore:line
 var rootaddress = "lhportal"; // vaihda tarpeen mukaan
 
+describe('Basic test suite', function() {
 
-describe('Test login page opens', function() {
-  it('User sees the title and the version number', function(done) {
-    this.timeout('30s')
-    var nightmare = Nightmare({show:false})
-    nightmare
-      .goto('http://localhost/' + rootaddress + '/index.php')
-      .evaluate(function () {
-        return document.title;
-      })
-      .end()
-      .then(function(title) {
-        expect(title).to.equal('Majakkaportaali 0.1');
-        done();
-      })
-  });
+    before(function() {
+        console.log("Aloitetaan testit. Kirjaudutaan sisään...");
+        this.timeout('30s');
+        this.nightmare = Nightmare({show:false});
+    });
+
+    after(function() {
+      console.log("Lopetetaan...");
+      // ...
+    });
+
+    describe('Test login page opens', function() {
+      it('User sees the title and the version number', function(done) {
+        this.nightmare
+          .goto('http://localhost/' + rootaddress + '/index.php')
+          .evaluate(function () {
+            return document.title;
+          })
+          .end()
+          .then(function(title) {
+            expect(title).to.equal('Majakkaportaali 0.1');
+            done();
+          })
+      });
+    });
+
 });
 
